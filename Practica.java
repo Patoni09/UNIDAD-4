a8a5bb41a9856bb668294a428b9edfe8d342b142
Patoni09 <143773059+Patoni09@users.noreply.github.com>
2023-10-27 09:00:56 -0600

    Add files via upload

---
+++ Practica.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Practica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double[]num ={20.5,40.6,78.4,94.7,55.3,22.6,84.3};
+         System.out.println("num" + num.length);
+         System.out.println("quieres sumar o multiplicar ");
+         double sum = num[0]+num[2]+num[3]+num[4]+num[5]+num[6]+num[7];
+        System.out.println("la suma de los arreglos es" + num);
+    }
+    
+}
